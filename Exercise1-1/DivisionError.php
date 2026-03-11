4f523ce2cfaddb70ff5192c6aef2cd747c632320
Kent Quinto <kentquinto1@gmail.com>
2026-03-11 13:14:13 +0100

    update files

--- Exercise1-1/DivisionError.php
+++ Exercise1-1/DivisionError.php
@@ -1,10 +1,13 @@
 <?php
-function division($a,$b) {
+function division(int $a, int $b) {
     if($b == 0) {
         throw new Exception("You cant divide a number by 0!");
     }
     return $a / $b;
 }
+
+echo division(6,2) . "\n";
+
 try {
     echo division(6,0);
 } catch (Exception $e) {
--- Exercise1-2/actionValidate.php
+++ Exercise1-2/actionValidate.php
@@ -1,41 +1,51 @@
 <?php
 session_start();
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $age = $_POST['age'];
+    $phone = $_POST['telephone'];
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$age = $_POST['age'];
+    echo "Form Data" . "\n";
+    echo "Name: " . $name . "\n";
+    echo "Email: " . $email . "\n";
+    echo "Age: " . $age . "\n";
+    echo "Telephone: " . $phone . "\n";
 
-echo "Form Data" . "\n";
-echo "Name: " . $name . "\n";
-echo "Email: " . $email . "\n";
-echo "Age: " . $age . "\n";
+    $_SESSION['username'] = $name;
+    $_SESSION['useremail'] = $email;
+    $_SESSION['userage'] = $age;
+    $_SESSION['userphone'] = $phone;
 
-$_SESSION['username'] = $name;
-$_SESSION['useremail'] = $email;
-$_SESSION['userage'] = $age;
-
-function validateName($name) {
-    if(empty($name) || !is_string($name)) {
-        throw new Exception("Name must contain only letters and cannot be empty.");
+    function validateName($name) {
+        if(empty($name) || !is_string($name)) {
+            throw new Exception("Name must contain only letters and cannot be empty!");
+        }
+        return $name;
     }
-    return $name;
-}
-function validateEmail($email) {
-    if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        throw new Exception("Invalid email!");
+    function validateEmail($email) {
+        if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception("Invalid email!");
+        }
+        return $email;
     }
-    return $email;
-}
-function validateAge($age) {
-    if (empty($age) || $age <= 0 || !is_numeric($age)) {
-        throw new Exception("Invalid age!");
+    function validateAge($age) {
+        if (empty($age) || $age <= 0 || !is_numeric($age)) {
+            throw new Exception("Invalid age!");
+        }
+        return $age;
     }
-    return $age;
+    function validatePhone($phone) {
+        if (empty($phone) || !preg_match("/^[0-9]{9}$/", $phone)) {
+            throw new Exception("Invalid phone number! It should be 9 digits!");
+        }
+        return $phone;
 }
+
 try {
     validateName($name);
     validateEmail($email);
     validateAge($age);
+    validatePhone($phone);
     echo "Validation successful!";
 } catch (Exception $exeception) {
     echo "Error! " . $exeception->getMessage();
