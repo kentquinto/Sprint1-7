1e1d2d26e3986d27a7f62562acd1afe23c216b1b
Kent Quinto <kentquinto1@gmail.com>
2026-03-18 13:12:31 +0100

    Refactor form validations and enhanced user input requirements

--- Exercise1-2/actionValidate.php
+++ Exercise1-2/actionValidate.php
@@ -5,24 +5,13 @@ session_start();
     $age = $_POST['age'];
     $phone = $_POST['telephone'];
 
-    echo "Form Data" . "\n";
-    echo "Name: " . $name . "\n";
-    echo "Email: " . $email . "\n";
-    echo "Age: " . $age . "\n";
-    echo "Telephone: " . $phone . "\n";
-
-    $_SESSION['username'] = $name;
-    $_SESSION['useremail'] = $email;
-    $_SESSION['userage'] = $age;
-    $_SESSION['userphone'] = $phone;
-
-    function validateName($name) {
-        if(empty($name) || !is_string($name)) {
-            throw new Exception("Name must contain only letters and cannot be empty!");
+    function validateName(string $name)  {
+        if (empty($name)) {
+            throw new Exception("Name must not be empty!");
         }
         return $name;
     }
-    function validateEmail($email) {
+    function validateEmail(string $email) {
         if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             throw new Exception("Invalid email!");
         }
@@ -40,14 +29,25 @@ session_start();
         }
         return $phone;
 }
-
 try {
-    validateName($name);
-    validateEmail($email);
-    validateAge($age);
-    validatePhone($phone);
+    $name = validateName($name);
+    $email = validateEmail($email);
+    $age = validateAge($age);
+    $phone = validatePhone($phone);
+
+    $_SESSION['username'] = $name;
+    $_SESSION['useremail'] = $email;
+    $_SESSION['userage'] = $age;
+    $_SESSION['userphone'] = $phone;
+
     echo "Validation successful!";
-} catch (Exception $exeception) {
-    echo "Error! " . $exeception->getMessage();
+
+} catch (Exception $exception) {
+    echo "Error! " . $exception->getMessage();
 }
+echo "Form Data:" . "<br>";
+echo "Name: " . $name . "<br>";
+echo "Email: " . $email . "<br>";
+echo "Age: " . $age . "<br>";
+echo "Telephone: " . $phone . "<br>";
 ?>
